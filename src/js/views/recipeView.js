17fac46710d3447f18eef6d5c65054589cfcbf8c
Khwaeesh-ashutec <khwaeesh@ashutec.com>
2023-01-17 10:47:04 +0530

    WOrking Fr

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { Fraction } from "fractional"
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
